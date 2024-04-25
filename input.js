83b78d16baf5fe71aecd96792d5f546ec1a82794
bleepblue <lawrence.charlesworth0@gmail.com>
2024-04-25 21:51:51 +0100

    fixed bug where snake could double back on itself

--- input.js
+++ input.js
@@ -5,19 +5,19 @@ window.addEventListener('keydown', e =>{
     switch(e.key)
     {
         case 'ArrowUp':
-            if(inputDirection.y !== 0) break
+            if(lastInputDirection.y !== 0) break
             inputDirection = {x: 0, y: -1}
             break
         case 'ArrowDown':
-            if(inputDirection.y !== 0) break
+            if(lastInputDirection.y !== 0) break
             inputDirection = {x: 0, y: 1}
             break
         case 'ArrowLeft':
-            if(inputDirection.x !== 0) break
+            if(lastInputDirection.x !== 0) break
             inputDirection = {x: -1, y: 0}
             break
         case 'ArrowRight':
-            if(inputDirection.x !== 0) break
+            if(lastInputDirection.x !== 0) break
             inputDirection = {x: 1, y: 0}
             break
     }
